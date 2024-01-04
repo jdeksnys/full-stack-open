526d1bb15a99997e5cb1a6513addab32351a4f3f
jdeksnys <jonasdeksnys@gamil.com>
2024-01-04 16:07:13 +0200

    pt_1_5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,30 +1,29 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
-  const courseObj = {
-    name: course,
-    parts: parts,
-  };
+
 
   return (
     <div>
-      <Header course={courseObj.name} />
-      <Content parts={courseObj.parts}/>
-      <Total parts={courseObj.parts}/>
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
