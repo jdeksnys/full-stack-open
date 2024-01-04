ab9160497b69affd45a711c211fb2da4b5f50caf
jdeksnys <jonasdeksnys@gamil.com>
2024-01-04 15:54:12 +0200

    pt_1_3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,27 +1,24 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   const courseObj = {
     name: course,
     parts: {
-      part1: {
-        name: part1,
-        exCount: exercises1,
-      },
-      part2: {
-        name: part2,
-        exCount: exercises2,
-      },
-      part3: {
-        name: part3,
-        exCount: exercises3,
-      },
+      part1: part1,
+      part2: part2,
+      part3: part3,
     },
   };
 
@@ -62,7 +59,7 @@ export function Total({courseObj}) {
   let tot = 0;
   
   for(let p in courseObj.parts){
-    tot += courseObj.parts[p].exCount;
+    tot += courseObj.parts[p].exercises;
   }
 
   return(<p>Number of exercises {tot}</p>);
@@ -72,6 +69,6 @@ export function Total({courseObj}) {
 
 export function Part({partObj}) {
   return (
-    <p>{partObj.name} {partObj.exCount}</p>
+    <p>{partObj.name} {partObj.exercises}</p>
   );
 }
