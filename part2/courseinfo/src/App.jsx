e199154ac69c14321e60ab5c436fbbf84c04085c
jdeksnys <jonasdeksnys@gamil.com>
2024-01-07 17:46:10 +0200

    pt_2_3&4

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -4,12 +4,13 @@ import { useState } from 'react'
 const Course = ({course}) => {
   return (
     <div>
-      <h1>Half Stack Application Development</h1>
+      <h2>{course.name}</h2>
       <ul>
-        {course.parts.map((part) => 
+        {course.parts.map(part => 
           <li key={part.id}>{part.name} {part.exercises}</li>
         )}
       </ul>
+      <Statistics course={course} />
     </div>
   )
 }
@@ -20,32 +21,61 @@ const Statistics = ({course}) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
-      <Statistics course={course} />
+      <h1>Web developmnet curriculum</h1>
+      <ul>
+        {courses.map(function(course) {
+          return (
+            <Course key={course.id} course={course} />
+          )}
+        )}
+      </ul>
     </div>
   )
 }
